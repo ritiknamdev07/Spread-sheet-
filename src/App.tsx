68e8e8dc1b2170a0592a9bf01bfca95e815cd3bd
ritik namdev <ritiknamdev5178@gmail.com>
2025-07-02 17:38:02 +0530

    Added Toolbar in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Toolbar from "./components/Topbar/Navbar";
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
+        <Toolbar />
       </div>
     </>
   );
---
+++ src/components/Topbar/Navbar.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { MdKeyboardDoubleArrowRight } from "react-icons/md";
+import { Button } from "../ui/button";
+import { BiHide } from "react-icons/bi";
+import { Separator } from "../ui/separator";
+import { TbArrowAutofitHeight } from "react-icons/tb";
+import { BiSortAlt2 } from "react-icons/bi";
+import { IoFilterOutline } from "react-icons/io5";
+import { LuArrowDownToLine } from "react-icons/lu";
+import { TiExportOutline } from "react-icons/ti";
+import { LuArrowUpToLine } from "react-icons/lu";
+import { TiArrowShuffle } from "react-icons/ti";
+const Toolbar: React.FC = () => {
+  return (
+    <div className=" border-b border-gray-300 h-[48px] flex items-center px-6 w-full gap-[8px] justify-between">
+      <div className="flex items-center gap-[8px]">
+        <Button
+          variant="ghost"
+          size="sm"
+          className="text-[14px]"
+          style={{ fontWeight: 400 }}
+        >
+          Tool bar <MdKeyboardDoubleArrowRight className="w-[16px] h-[16px]" />
+        </Button>{" "}
+      <div className="h-[24px]">
+        {" "}
+        <Separator orientation="vertical" />
+      </div>
+      <div>
+        {" "}
+        <Button
+          variant="ghost"
+       
+          className="text-[14px] h-[36px]"
+          style={{ fontWeight: 400 }}
+        >
+          <BiHide /> Hide fields
+        </Button>
+      </div>
+      <div>
+        {" "}
+        <Button
+          variant="ghost"
+          
+          className="text-[14px]"
+          style={{ fontWeight: 400 }}
+        >
+          <BiSortAlt2 size={20} /> Sort
+        </Button>
+      </div>
+      <div>
+        {" "}
+        <Button
+          variant="ghost"
+          
+          className="text-[14px]"
+          style={{ fontWeight: 400 }}
+        >
+          <IoFilterOutline size={20} /> Filter
+        </Button>
+      </div>
+      <div>
+        {" "}
+        <Button
+          variant="ghost"
+          
+          className="text-[14px]"
+          style={{ fontWeight: 400 }}
+        >
+          <TbArrowAutofitHeight size={20} /> Cell view
+        </Button>
+      </div>
+      </div>
+     
+      <div className="flex items-center gap-[8px]">
+        <Button
+          variant="outline"
+          
+          className="text-[14px]"
+          style={{ fontWeight: 400 }}
+        >
+          <LuArrowDownToLine size={20} /> Import
+        </Button>
+        <Button
+          variant="outline"
+          
+          className="text-[14px]"
+          style={{ fontWeight: 400 }}
+        >
+          <LuArrowUpToLine size={20} /> Export
+        </Button>
+        <Button
+          variant="outline"
+          
+          className="text-[14px]"
+          style={{ fontWeight: 400 }}
+        >
+          <TiExportOutline size={20} /> Share
+        </Button>
+        <Button
+          
+
+          
+          className="text-[14px] bg-[#4B6A4F]"
+          style={{ fontWeight: 400 }}
+        >
+          <TiArrowShuffle size={20} /> New Action
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Toolbar;
